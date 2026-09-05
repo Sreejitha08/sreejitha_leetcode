1f1271c52e9000e452574fae274e534865aed4a0
DASU SREEJITHA <dasusreejitha@gmail.com>
2026-09-05 17:54:52 +0530

    Time: 55 ms (7.28%), Space: 19.1 MB (64.52%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        res=0
+        for i in range(32):
+            b=n&1
+            res=(res<<1)|b
+            n=n>>1
+        return res
